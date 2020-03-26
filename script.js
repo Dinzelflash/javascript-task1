09560098f772f6fefc54b79f9a7a62c9817d1d23
Ndiyo Daniel <61994904+Dinzelflash@users.noreply.github.com>
2020-03-26 20:31:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var name = "Ndiyo Daniel"
+var courses = ["HTML,CSS,JavaScript"]
+console.log("Name: "+name+"")
+console.log("Courses: "+courses+"")
+console.log("Odd numbers from 1-200 are: ");
+for (i=1; i<=200; i++){
+   if (i % 2 != 0){
+        console.log(i);
+    }
+}
